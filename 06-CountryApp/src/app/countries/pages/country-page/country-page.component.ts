2ca23bde6fe2979843dc5f3617353fea88c8923c
Alejandro González <17947093+magvdev@users.noreply.github.com>
2023-11-15 19:47:38 +0000

    UPD: finalizado country-page

--- 06-CountryApp/src/app/countries/pages/country-page/country-page.component.ts
+++ 06-CountryApp/src/app/countries/pages/country-page/country-page.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -14,6 +16,9 @@ export class CountryPageComponent implements OnInit {
 	private router = inject(Router);
 	private countriesService = inject(CountriesService);
 
+	public country?: Country;
+	public loading = true;
+
 	ngOnInit(): void {
 		this.activatedRoute.params
       .pipe(
@@ -21,11 +26,10 @@ export class CountryPageComponent implements OnInit {
           this.countriesService.searchCountryByAplhaCode(id)
         )
       )
-      .subscribe((country) => {
+      .subscribe((country) => {				
 				if (!country) return this.router.navigateByUrl('');
-
-        console.log(country);
-				return;
+				this.loading = false;				
+        return this.country = country;
       });
 	}
 }
